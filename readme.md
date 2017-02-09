4f0b1f521be72373d4a1a7a19d2845dd9faf0701
Michael LaRoy <mike.laroy@gmail.com>
2017-02-08 22:38:34 -0800

    initial commit

---
+++ readme.md
@@ -0,0 +1,25 @@
+# Weather Widget
+A combination of [Bootstrap's Weather Icons](http://erikflowers.github.io/weather-icons) and Yahoo's Weather API.
+
+While the entire icon font is included, this application only uses a subset of the icons based on the weather codes provided by [Yahoo's API](https://developer.yahoo.com/weather/documentation.html#codes)
+
+## Usage
+Make sure to include the stylesheet which references the font files:
+
+```
+<link rel="stylesheet" src="weather-icons.css">
+```
+
+and initialize the weather after including the JavaScript. The function takes a [WOEID](http://woeid.rosselliot.co.nz/lookup/) (Where On Earth IDentifier) parameter as a string.
+
+```
+<script src="weather.js"></script>
+<script>
+    // const vancouver = '9807';
+    weather( vancouver );
+</script>
+```
+
+
+### Notes
+Make sure you read about [rate limits](https://developer.yahoo.com/weather/#ratelimits) and [attribution guidelines](https://developer.yahoo.com/attribution/)
---
+++ src/weather.js
@@ -0,0 +1,50 @@
+function weather( cityId ) {
+
+  const container = document.getElementById('weather');
+  const base_url = 'http://query.yahooapis.com/v1/public/yql';
+  const yql_query = encodeURIComponent(`select * from weather.forecast where woeid=${cityId} AND u="c"`);
+  const yql_query_url = `${base_url}?q=${yql_query}&format=json`;
+
+  // send request to Yahoo
+  let xhr = new XMLHttpRequest();
+  xhr.overrideMimeType('application/json');
+  xhr.open('GET', yql_query_url, true);
+
+  xhr.onreadystatechange = function () {
+    if (xhr.readyState == 4 && xhr.status == '200') {
+      getCurrentWeather(xhr.responseText);
+    }
+  };
+  xhr.send();
+
+
+  function getCurrentWeather(data){
+
+    let weatherData = JSON.parse(data);
+
+    // get local json of weather codes
+    let xobj = new XMLHttpRequest();
+    xobj.overrideMimeType('application/json');
+    xobj.open('GET', 'weathercodes.json', true);
+
+    xobj.onreadystatechange = function () {
+      if (xobj.readyState == 4 && xobj.status == '200') {
+        render(weatherData, xobj.responseText);
+      }
+    };
+    xobj.send();
+  }
+
+  function render(weather, icons){
+    const iconsData = JSON.parse(icons);
+    const description = weather.query.results.channel.item.description;
+    const condition = weather.query.results.channel.item.condition;
+    const result = iconsData['codes'].filter(function(item){
+      return item.number == condition.code;
+    });
+
+    container.innerHTML = `<p class="weather type-small">
+      <i class="wi ${result[0].class}"></i>
+      Currently ${condition.temp} &deg;C., and ${result[0].description} in Vancouver</p>`;
+  }
+}
